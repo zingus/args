20593a45d8a8b96d12dd7952373d9e90a9f8d1e2
zingus <zingus@gmail.com>
2018-02-02 14:09:21 +0100

    license

--- args-php-test.php
+++ args-php-test.php
@@ -1,7 +1,27 @@
 <?php
-require_once 'args.php';
+require_once 'args.wip.php';
 
 $args=new args();
 $args->debug=true;
 $args->process();
 
+
+/*
+$args->parseCommandLine("./command.sh ok")
+
+array (
+  0 => './command.sh',
+  1 => 'ok',
+);
+*/
+
+/*?PRINT_R $args->parseCommandLine("./command.sh o\\k");
+Array
+(
+    [0] => ./command.sh
+    [1] => ok
+)
+*/
+
+$argv=$args->parseWindowsCommandLine("command.exe ^ok");
+var_export($argv);
